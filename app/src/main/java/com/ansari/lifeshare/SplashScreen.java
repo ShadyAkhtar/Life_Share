87eddd1587351d66085ad92d2e0335dd76da86eb
Aishah <aishah.ansari00@gmail.com>
2020-10-07 23:13:57 +0530

    Initial commit

---
+++ app/src/main/java/com/ansari/lifeshare/SplashScreen.java
@@ -0,0 +1,41 @@
+package com.ansari.lifeshare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import static com.ansari.lifeshare.R.*;
+import static com.ansari.lifeshare.R.anim.*;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private static int SPLASH_TIMER = 5000;
+    Animation sideAnim;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(layout.splash_screen);
+
+        ImageView appImage = findViewById(id.appIcon);
+        TextView appName = findViewById(id.appName);
+
+        sideAnim = AnimationUtils.loadAnimation(this, side_anim);
+
+        appImage.setAnimation(sideAnim);
+
+        new Handler().postDelayed(new Runnable(){
+        public void run(){
+            Intent intent = new Intent(SplashScreen.this,UserDashboard.class);
+                    startActivity(intent);
+        }
+        },SPLASH_TIMER);
+    }
+}
