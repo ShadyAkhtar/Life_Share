3096bd814e9a3cb9fb7ff1017474cfac57bc6a13
Shadab <akhtarshadab415@gmail.com>
2020-11-10 22:53:39 +0530

    working on validation

--- app/src/main/java/com/ansari/lifeshare/User/SignUp.java
+++ app/src/main/java/com/ansari/lifeshare/User/SignUp.java
@@ -1,16 +1,43 @@
 package com.ansari.lifeshare.User;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.ImageView;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.Toast;
 
+import com.ansari.lifeshare.Common.LoginSignup.VerifyOTP;
 import com.ansari.lifeshare.R;
+import com.google.android.material.textfield.TextInputLayout;
 
-public class SignUp extends AppCompatActivity {
+import java.util.Calendar;
+
+public class SignUp extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     ImageView menuBack;
+    Spinner spinnerBldGrp;
+    DatePicker datePicker;
+
+    //variables
+
+    Button btnSignup;
+    RadioGroup btnGender;
+
+    //get data variables
+
+    TextInputLayout username,password,confirmPassword,email,phoneNo;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,9 +45,25 @@ public class SignUp extends AppCompatActivity {
         setContentView(R.layout.activity_sign_up);
 
 
+        //hooks for button
+        btnSignup = findViewById(R.id.btnSignup);
+        username =findViewById(R.id.username);
+        email =  findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        confirmPassword = findViewById(R.id.confirmPassword);
+        btnGender = findViewById(R.id.btnGender);
+        phoneNo = findViewById(R.id.phoneNo);
+        datePicker = findViewById(R.id.age_picker);
+
+
         menuBack = findViewById(R.id.menu_back);
 
+        spinnerBldGrp = findViewById(R.id.spinner_bld_grp);
 
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.bloodgroup, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerBldGrp.setAdapter(adapter);
+        spinnerBldGrp.setOnItemSelectedListener(this);
 
         menuBack.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,6 +71,153 @@ public class SignUp extends AppCompatActivity {
                 SignUp.super.onBackPressed();
             }
         });
+    }
+
+    public void callNextSignupScreen(View view){
+
+        if ( !validateUsername() | !validateEmail() | !validatePassword() | !validateConfirmPassword()) {
+            return;
+        }
+        Intent intent = new Intent(getApplicationContext(), VerifyOTP.class);
+        startActivity(intent);
+    }
+
+    private boolean validateUsername() {
+        String val = username.getEditText().getText().toString().trim();
+        String checkspaces = "\\A\\w{1,20}\\z";
+
+        if (val.isEmpty()) {
+            username.setError("Field can not be empty");
+            return false;
+        } else if (val.length() > 20) {
+            username.setError("Username is too large!");
+            return false;
+        } else if (!val.matches(checkspaces)) {
+            username.setError("No White spaces are allowed!");
+            return false;
+        } else {
+            username.setError(null);
+            username.setErrorEnabled(false);
+            return true;
+        }
+    }
+
+    private boolean validateEmail() {
+        String val = email.getEditText().getText().toString().trim();
+        String checkEmail = "[a-zA-Z0-9._-]+@[a-z]+.+[a-z]+";
+
+        if (val.isEmpty()) {
+            email.setError("Field can not be empty");
+            return false;
+        } else if (!val.matches(checkEmail)) {
+            email.setError("Invalid Email!");
+            return false;
+        } else {
+            email.setError(null);
+            email.setErrorEnabled(false);
+            return true;
+        }
+    }
+
+    private boolean validatePassword() {
+        String val = password.getEditText().getText().toString().trim();
+        String checkPassword = "^" +
+                "(?=.*[0-9])" +         //at least 1 digit
+                //"(?=.*[a-z])" +         //at least 1 lower case letter
+                //"(?=.*[A-Z])" +         //at least 1 upper case letter
+                "(?=.*[a-zA-Z])" +      //any letter
+               // "(?=.*[@#$%^&+=])" +    //at least 1 special character
+                "(?=\\S+$)" +           //no white spaces
+                ".{4,}" +               //at least 4 characters
+                "$";
+
+        if (val.isEmpty()) {
+            password.setError("Field can not be empty");
+            return false;
+        } else if (!val.matches(checkPassword)) {
+            password.setError("Password should contain 4 characters!");
+            return false;
+        } else {
+            password.setError(null);
+            password.setErrorEnabled(false);
+            return true;
+        }
+    }
+
+    private boolean validateConfirmPassword() {
+        String val = confirmPassword.getEditText().getText().toString().trim();
+        String checkPassword = "^" +
+                "(?=.*[0-9])" +         //at least 1 digit
+                //"(?=.*[a-z])" +         //at least 1 lower case letter
+                //"(?=.*[A-Z])" +         //at least 1 upper case letter
+                "(?=.*[a-zA-Z])" +      //any letter
+                //"(?=.*[@#$%^&+=])" +    //at least 1 special character
+                "(?=\\S+$)" +           //no white spaces
+                ".{4,}" +               //at least 4 characters
+                "$";
+
+        if (val.isEmpty()) {
+            confirmPassword.setError("Field can not be empty");
+            return false;
+        } else if (!val.matches(checkPassword)) {
+            confirmPassword.setError("Password should contain 4 characters!");
+            return false;
+        } else {
+            confirmPassword.setError(null);
+            confirmPassword.setErrorEnabled(false);
+            return true;
+        }
+    }
+
+    private boolean validateGender() {
+        if (btnGender.getCheckedRadioButtonId() == -1) {
+            Toast.makeText(this, "Please Select Gender", Toast.LENGTH_SHORT).show();
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    private boolean validateAge() {
+        int currentYear = Calendar.getInstance().get(Calendar.YEAR);
+        int userAge = datePicker.getYear();
+        int isAgeValid = currentYear - userAge;
+
+        if (isAgeValid < 14) {
+            Toast.makeText(this, "You are not eligible to apply", Toast.LENGTH_SHORT).show();
+            return false;
+        } else
+            return true;
+    }
+
+    private boolean validatePhoneNumber() {
+        String val = phoneNo.getEditText().getText().toString().trim();
+        String checkspaces = "Aw{1,20}z";
+        if (val.isEmpty()) {
+            phoneNo.setError("Enter valid phone number");
+            return false;
+        } else if (!val.matches(checkspaces)) {
+            phoneNo.setError("No White spaces are allowed!");
+            return false;
+        } else {
+            phoneNo.setError(null);
+            phoneNo.setErrorEnabled(false);
+            return true;
+        }
+    }
+
+
+
+
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long l) {
+        String text = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
 
     }
 }
